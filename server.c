17c45af84112d63de6caecd4f86c603c66f4ce7e
rakim0 <rakimmiddya@protonmail.com>
2023-08-08 00:42:34 +0530

    Moved some functions from server.c to main_memory.c

--- server.c
+++ server.c
@@ -1,20 +1,21 @@
-#include <stdio.h>
-#include "student.h"
-#include <stdlib.h>
-
 #ifndef SERVER
 #define SERVER
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "student.h"
+#include "main_memory.c"
 
 // Functional Prototypes
 void server();
 void registerStudent();
-int search(int id);
-void update(int id);
+
 void delete(int id);
 void printFile(char* filename);
 
 void server()
 {
+    node *head = NULL;
     char op_id = 0; // OPERATION ID, 0->registration, 1-> search, 2-> update, 3-> delete.
     int st_id = 0;
     while(1)
@@ -30,18 +31,23 @@ void server()
 
             case '1':
                 scanf("%d", &st_id);
-                search(st_id);
+                search(&head, st_id);
                 break;
 
             case '2':
                 scanf("%d", &st_id);
-                update(st_id);
+                int newRoomNumber = 0;
+                scanf("%d\n", &newRoomNumber);
+                update(&head, st_id, newRoomNumber);
                 break;
                 
             case'3':
                 scanf("%d", &st_id);
                 delete(st_id);
                 //printFile("disk.tmp");
+                break;
+            case '4':
+                printFile("disk");
                 break;
         }
     }
@@ -52,79 +58,25 @@ void server()
 // Use printFile to see if the file is updated.
 void registerStudent()
 {
-    FILE* db = fopen("disk", "r");
-    if (db == NULL) 
-    {
-        perror("Error opening database file");
-        exit(0);
-    }
-    fclose(db);
-    printFile("disk");
-}
-
-// Search the file and return 1 if found and 0 if not
-int search(int id)
-{
-    FILE* db = fopen("disk", "r");
-    if (db == NULL) 
-    {
-        perror("Error opening database file");
-        exit(0);
-    }
-    struct Student temp;
-    while(fread(&temp, 1, sizeof(struct Student), db) && sizeof(temp) == sizeof(struct Student))
-    {
-        if (temp.id == id)
-        {
-            printf("%d, %s, %s, %s, %d, %s, Year %d\n", temp.id, temp.name, 
-                temp.hostel, temp.course, temp.roomNumber, temp.dob, temp.yearOfStudy);
-                fclose(db);
-            return 1;
-        }
-    }
-    
-    printf("NOT FOUND!!\n");
-    fclose(db);
-    return 0;
-}
-
-// Update the file.
-void update(int id)
-{
-    /* 
-     *
-     *  Implement the logic for updating here.
-     *  First search file, but instead of returning a number, get the file pointer.
-     *  Use fseek to reposition the file pointer. The number of bytes will be similar.
-     *  And write the new struct there.
-     *
-     */
     FILE* db = fopen("disk", "r+");
     if (db == NULL) 
     {
         perror("Error opening database file");
         exit(0);
     }
-
-    int newRoomNumber = 0;
-    scanf("%d\n", &newRoomNumber);
-
     struct Student temp;
+    int id = 0;
 
     while(fread(&temp, 1, sizeof(struct Student), db) && sizeof(temp) == sizeof(struct Student))
     {
-        if (temp.id == id)
-        {
-            temp.roomNumber = newRoomNumber;
-            fseek(db, -sizeof(struct Student), SEEK_CUR);
-            fwrite(&temp, 1, sizeof(struct Student), db);
-            fclose(db);
-            return;
-        }
+        id = temp.id;
     }
 
+    struct Student s;
+    id++;
+    s = generateStudent(id);
+    fwrite(&s, 1, sizeof(struct Student), db);
     fclose(db);
-    return;
 }
 
 // Delete a line from the file 
@@ -175,7 +127,8 @@ void delete(int id)
 void printFile(char* filename)
 {
     FILE* db = fopen(filename, "r");
-    if (db == NULL) {
+    if (db == NULL) 
+    {
         perror("Error opening database file");
         exit(0);
     }
