80f2f9234869a587892f78a90c751182d67fccea
rakim0 <rakimmiddya@protonmail.com>
2023-08-08 23:46:25 +0530

    [Makefile] added a targer for server_user and deleting all files

--- generate_random.c
+++ generate_random.c
@@ -2,38 +2,69 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+#include <string.h>
+#include "tuple.h"
+
+unsigned int randval()
+{
+    unsigned int randval;
+    FILE *f;
+    f = fopen("/dev/random", "r");
+    fread(&randval, sizeof(randval), 1, f);
+    fclose(f);
+    return randval;
+}
 
 int main()
 {
     srand(time(NULL));
-    FILE *queue_file = fopen("operation_queue.txt", "a");
+    FILE *queueFile = fopen("operation_queue.txt", "a");
+    FILE *waitingQueue = fopen("waiting_queue", "a");
+    if (queueFile == NULL) 
+    {
+        perror("Error opening file");
+        return 1;
+    }
+    if (waitingQueue == NULL) 
+    {
+        perror("Error opening file");
+        return 1;
+    }
+
     for (int i = 0; i < 20; i++)
     {
-        if (queue_file == NULL) 
+        
+        int studentId = randval() % 100 + 1;
+        int operationId = randval() % 4;
+
+        tuple temp = {};
+        temp.stdID = studentId;
+        temp.opID = operationId;
+        strcpy(temp.type, "random");
+        if (operationId == 0)
         {
-            perror("Error opening file");
-            return 1;
+            fwrite(&temp, 1, sizeof(tuple), waitingQueue);
+            fprintf(queueFile, "(random, %d, %ld)\n", operationId, time(NULL));
         }
-        int student_id = rand() % 100 + 1;
-        int operation_id = rand() % 4;
-        
-        
-        
-        if (operation_id == 2) 
+        else if (operationId == 2) 
         {
-            int hostel_room = rand() % 100 + 101;
-            fprintf(queue_file, "(random, %d, %d, %d, %ld)\n", student_id, operation_id, hostel_room, time(NULL));
+            int hostelRoom = randval() % 100 + 101;
+            temp.roomNo = hostelRoom;
+            fwrite(&temp, 1, sizeof(tuple), waitingQueue);
+            fprintf(queueFile, "(random, %d, %d, %d, %ld)\n", operationId, studentId, hostelRoom, time(NULL));
         }
 
         else
         {
-            fprintf(queue_file, "(random, %d, %d, %ld)\n", student_id, operation_id, time(NULL));
-        }
-        
-        double random_interval = rand()%3 + 1;
-        sleep(random_interval);
-        fflush(queue_file);
+            fwrite(&temp, 1, sizeof(tuple), waitingQueue);
+            fprintf(queueFile, "(random, %d, %d, %ld)\n", operationId, studentId, time(NULL));
+        }        
+        double randomInterval = randval()%3 + 1;
+        fflush(queueFile);
+        fflush(waitingQueue);
+        sleep(randomInterval);
     }
-    fclose(queue_file);
+    fclose(queueFile);
+    fclose(waitingQueue);
     return 0;
 }
--- generate_regular.c
+++ generate_regular.c
@@ -1,37 +1,70 @@
-/*
-* Cecily
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+#include <string.h>
+#include "tuple.h"
+
+unsigned int randval()
+{
+    unsigned int randval;
+    FILE *f;
+    f = fopen("/dev/random", "r");
+    fread(&randval, sizeof(randval), 1, f);
+    fclose(f);
+    return randval;
+}
 
 int main()
 {
-    //srand(time(NULL));
-    FILE *queue_file = fopen("operation_queue.txt", "a");
-    if (queue_file == NULL) 
+    srand(time(NULL));
+    FILE *queueFile = fopen("operation_queue.txt", "a");
+    FILE *waitingQueue = fopen("waiting_queue", "a");
+    if (queueFile == NULL) 
     {
         perror("Error opening file");
         return 1;
     }
-    for (int i = 0; i < 50; i++) 
+    if (waitingQueue == NULL) 
     {
-        int student_id = rand() % 100 + 1;
-        int operation_id = rand() % 4;
-        if (operation_id == 2)
+        perror("Error opening file");
+        return 1;
+    }
+
+    for (int i = 0; i < 50; i++)
+    {
+        
+        int studentId = randval() % 100 + 1;
+        int operationId = randval() % 4;
+
+        tuple temp = {};
+        temp.stdID = studentId;
+        temp.opID = operationId;
+        strcpy(temp.type, "regular");
+        if (operationId == 0)
         {
-            int hostel_room = rand() % 100 + 101;
-            fprintf(queue_file, "(regular, %d, %d, %d, %ld)\n", student_id, operation_id, hostel_room, time(NULL));
+            fwrite(&temp, 1, sizeof(tuple), waitingQueue);
+            fprintf(queueFile, "(regular, %d, %ld)\n", operationId, time(NULL));
         }
+        else if (operationId == 2) 
+        {
+            int hostelRoom = randval() % 100 + 101;
+            temp.roomNo = hostelRoom;
+            fwrite(&temp, 1, sizeof(tuple), waitingQueue);
+            fprintf(queueFile, "(regular, %d, %d, %d, %ld)\n", operationId, studentId, hostelRoom, time(NULL));
+        }
+
         else
         {
-            fprintf(queue_file, "(regular, %d, %d, %ld)\n", student_id, operation_id, time(NULL));
+            fwrite(&temp, 1, sizeof(tuple), waitingQueue);
+            fprintf(queueFile, "(regular, %d, %d, %ld)\n", operationId, studentId, time(NULL));
         }
-        fflush(queue_file);
+        
+        fflush(queueFile);
+        fflush(waitingQueue);
         sleep(1);
     }
-    fclose(queue_file);
+    fclose(queueFile);
+    fclose(waitingQueue);
     return 0;
 }
